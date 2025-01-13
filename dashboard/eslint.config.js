7ff0b0519ae001179321a6dae8d70451d8dccce9
karoline <karoline.wahl@hotmail.no>
2025-01-13 12:20:14 +0100

    Formating and linting works.

--- dashboard/eslint.config.js
+++ dashboard/eslint.config.js
@@ -4,13 +4,15 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import react from 'eslint-plugin-react'
-import eslintConfigPrettier from "eslint-config-prettier";
-
+import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default tseslint.config(
-  { ignores: ['dist'] },  
+  { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommendedTypeChecked],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommendedTypeChecked,
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
@@ -36,5 +38,5 @@ export default tseslint.config(
         { allowConstantExport: true },
       ],
     },
-  },
+  }
 )
