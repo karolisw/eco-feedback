0a3b60fb8b0b176809351791f062d8c49035db91
karoline <karoline.wahl@hotmail.no>
2025-01-15 11:37:10 +0100

    Show the thrust and setpoint.

---
+++ dashboard/src/components/Thruster.tsx
@@ -0,0 +1,26 @@
+import { ObcThruster } from '@oicl/openbridge-webcomponents-react/navigation-instruments/thruster/thruster'
+
+type ThrusterProps = {
+  thrust: number //  The thrust of the thruster in percent (0-100)
+  setPoint: number | undefined // The setpoint of the thruster in percent (0-100)
+  touching: boolean // Whether the thruster is being touched by the operator'
+  atSetpoint: boolean // Whether the thruster is at the setpoint
+}
+
+export function Thruster({
+  thrust,
+  setPoint,
+  atSetpoint,
+  touching,
+}: ThrusterProps) {
+  return (
+    <div>
+      <ObcThruster
+        thrust={thrust}
+        setpoint={setPoint}
+        touching={touching}
+        atSetpoint={atSetpoint}
+      ></ObcThruster>
+    </div>
+  )
+}
