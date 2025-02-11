1be8dd16b58298c374736a80947363e5083dd2ab
karoline <karoline.wahl@hotmail.no>
2025-02-11 12:35:28 +0100

    Dashboard shows azimuth, thruster and rudder in separate tabs.

--- dashboard/src/pages/Dashboard.tsx
+++ dashboard/src/pages/Dashboard.tsx
@@ -1,8 +1,11 @@
 import { Rudder } from '../components/Rudder'
-import { Emissions } from '../components/Emissions'
+import { AzimuthThruster } from '../components/AzimuthThruster'
 import { Thruster } from '../components/Thruster'
 import { UseWebSocket } from '../hooks/useWebSocket'
 import { memo } from 'react'
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs'
+import 'react-tabs/style/react-tabs.css'
+import '../styles/dashboard.css'
 
 type DashboardData = {
   currentThrust: number
@@ -13,7 +16,7 @@ type DashboardData = {
 }
 
 export function Dashboard() {
-  const targetEmissions = 30 //TODO unsure if targetEmissions should originate from the server
+  //const targetEmissions = 30 //TODO unsure if targetEmissions should originate from the server
   const initialData: DashboardData = {
     currentThrust: 20,
     currentAngle: 80, // Between -90 and 90
@@ -26,28 +29,35 @@ export function Dashboard() {
 
   //console.log(data)
   return (
-    <div>
-      <div>
-        <h1>Ship Dashboard</h1>
-        <MemoizedThruster
-          thrust={data.data.currentThrust}
-          angle={data.data.currentAngle}
-          setPoint={10}
-          touching={true}
-          atThrustSetpoint={false}
-          atAngleSetpoint={false}
-        />
-        <MemoizedRudder angle={data.data.currentAngle} />
-        <MemoizedEmissions
-          currentEmissions={data.data.currentEmissions}
-          targetEmissions={targetEmissions}
-        />
-      </div>
+    <div className="dashboard">
+      <Tabs>
+        <TabPanel>
+          <MemoizedAzimuthThruster
+            thrust={data.data.currentThrust}
+            angle={data.data.currentAngle}
+            setPoint={10}
+            touching={true}
+            atThrustSetpoint={false}
+            atAngleSetpoint={false}
+          />
+        </TabPanel>
+        <TabPanel>
+          <MemoizedRudder angle={data.data.currentAngle} />
+        </TabPanel>
+        <TabPanel>
+          <MemoizedThruster thrust={data.data.currentThrust} />
+        </TabPanel>
+        <TabList>
+          <Tab>Azimuth</Tab>
+          <Tab>Rudder</Tab>
+          <Tab>Thruster</Tab>
+        </TabList>
+      </Tabs>
     </div>
   )
 }
 
 // Memoized Components to prevent unnecessary re-renders
+const MemoizedAzimuthThruster = memo(AzimuthThruster)
 const MemoizedThruster = memo(Thruster)
 const MemoizedRudder = memo(Rudder)
-const MemoizedEmissions = memo(Emissions)
