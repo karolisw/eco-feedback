8264082de5b1e90cbc1d339bc56c8ff01afd3b38
karoline <karoline.wahl@hotmail.no>
2025-02-11 12:34:55 +0100

    Thruster component.

--- dashboard/src/components/Thruster.tsx
+++ dashboard/src/components/Thruster.tsx
@@ -1,33 +1,16 @@
-//import { ObcThruster } from '@oicl/openbridge-webcomponents-react/navigation-instruments/thruster/thruster'
-import { ObcAzimuthThrusterLabeled } from '@oicl/openbridge-webcomponents-react/navigation-instruments/azimuth-thruster-labeled/azimuth-thruster-labeled'
-
+// Import thruster from
+import { ObcThruster } from '@oicl/openbridge-webcomponents-react/navigation-instruments/thruster/thruster'
+import '../styles/dashboard.css'
+// Create Thruster and thrust props
 type ThrusterProps = {
   thrust: number //  The thrust of the thruster in percent (0-100)
-  angle: number // The angle of the propeller (?)
-  setPoint: number | undefined // The setpoint of the thruster in percent (0-100)
-  touching: boolean // Whether the thruster is being touched by the operator'
-  atThrustSetpoint: boolean // Whether the thruster is at the setpoint
-  atAngleSetpoint: boolean // Whether the angle is at the setpoint
 }
 
-export function Thruster({
-  thrust,
-  angle,
-  setPoint,
-  touching,
-  atThrustSetpoint,
-  atAngleSetpoint
-}: ThrusterProps) {
+// Create Thruster component
+export function Thruster({ thrust }: ThrusterProps) {
   return (
-    <div>
-      <ObcAzimuthThrusterLabeled
-        thrust={thrust}
-        angle={angle}
-        thrustSetpoint={setPoint}
-        touching={touching}
-        atThrustSetpoint={atThrustSetpoint}
-        atAngleSetpoint={atAngleSetpoint}
-      ></ObcAzimuthThrusterLabeled>
+    <div className="dashboard-component">
+      <ObcThruster thrust={thrust}></ObcThruster>
     </div>
   )
 }
