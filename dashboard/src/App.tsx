fb6d31166c108ef333f0eca95f42d168b1e3f14d
karoline <karoline.wahl@hotmail.no>
2025-02-18 15:15:45 +0100

    Route to dashboard (simulator and feedback interface) and overhead display.

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -1,28 +1,16 @@
-import './App.css'
-//import { Routes, Route, Link } from 'react-router-dom'
-import { Dashboard } from './pages/Dashboard'
 import '@oicl/openbridge-webcomponents/src/palettes/variables.css' //TODO was this correct?
+import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import { Dashboard } from './pages/Dashboard'
+import { MiniDashboard } from './pages/MiniDashboard'
 
 function App() {
   return (
     <div>
-      {/*
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Dashboard</Link>
-          </li>
-          <li>
-            <Link to="/another">Another Page</Link>
-          </li>
-        </ul>
-      </nav>
-      
       <Routes>
         <Route path="/" element={<Dashboard />} />
+        <Route path="/mini" element={<MiniDashboard />} />
       </Routes>
-      */}
-      <Dashboard></Dashboard>
     </div>
   )
 }
