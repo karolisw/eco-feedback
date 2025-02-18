908a4ad616aef1c6763e7b6e58e2c1fc6531b5c2
karoline <karoline.wahl@hotmail.no>
2025-02-18 15:22:15 +0100

    Dashboard and simulator are communicating through websockets. Dashboard has also been styled using OICL components + css.

--- dashboard/src/pages/Dashboard.tsx
+++ dashboard/src/pages/Dashboard.tsx
@@ -1,22 +1,15 @@
-import { Rudder } from '../components/Rudder'
 import { AzimuthThruster } from '../components/AzimuthThruster'
-import { Thruster } from '../components/Thruster'
+import { Compass } from '../components/Compass'
+import { InstrumentField } from '../components/InstrumentField'
 import { UseWebSocket } from '../hooks/useWebSocket'
-import { memo } from 'react'
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs'
-import 'react-tabs/style/react-tabs.css'
+import { UseSimulatorWebSocket } from '../hooks/useSimulatorWebSocket'
+import { memo, useEffect } from 'react'
 import '../styles/dashboard.css'
-
-type DashboardData = {
-  currentThrust: number
-  currentAngle: number
-  consumption: number
-  currentEmissions: number
-  ecoScore: number
-}
+import '../styles/instruments.css'
+import { DashboardData, SimulatorData } from '../types/DashboardData'
+import { toHeading, gramsToKiloGrams } from '../utils/Convertion'
 
 export function Dashboard() {
-  //const targetEmissions = 30 //TODO unsure if targetEmissions should originate from the server
   const initialData: DashboardData = {
     currentThrust: 20,
     currentAngle: 80, // Between -90 and 90
@@ -25,39 +18,166 @@ export function Dashboard() {
     ecoScore: 100
   }
 
-  const data = UseWebSocket('ws://127.0.0.1:8000/ws', initialData)
+  const initialSimData: SimulatorData = {
+    heading: 90,
+    speed: 0,
+    rpm: 0,
+    xPos: -1.0,
+    yPos: 0.0,
+    resistance: 0,
+    consumptionRate: 0,
+    consumedTotal: 0,
+    checkpoints: 3,
+    power: 0, // Power of azimuth thruster
+    angle: 0 // Angle of azimuth thruster
+  }
 
-  //console.log(data)
+  const data = UseWebSocket('ws://127.0.0.1:8000/ws', initialData)
+  const simulatorData = UseSimulatorWebSocket(
+    'ws://127.0.0.1:8003',
+    initialSimData
+  )
+  // Store WebSocket data globally
+  useEffect(() => {
+    window.azimuthControllerData = data.data // Global variable for simulator
+  }, [data])
+
   return (
     <div className="dashboard">
-      <Tabs>
-        <TabPanel>
-          <MemoizedAzimuthThruster
-            thrust={data.data.currentThrust}
-            angle={data.data.currentAngle}
-            setPoint={10}
-            touching={true}
-            atThrustSetpoint={false}
-            atAngleSetpoint={false}
-          />
-        </TabPanel>
-        <TabPanel>
-          <MemoizedRudder angle={data.data.currentAngle} />
-        </TabPanel>
-        <TabPanel>
-          <MemoizedThruster thrust={data.data.currentThrust} />
-        </TabPanel>
-        <TabList>
-          <Tab>Azimuth</Tab>
-          <Tab>Rudder</Tab>
-          <Tab>Thruster</Tab>
-        </TabList>
-      </Tabs>
+      {/* Simulator Panel */}
+      <div className="simulator-panel">
+        <iframe src="http://127.0.0.1:8002/index.html" />
+      </div>
+      <div className="ui-panel">
+        <h3>Propulsion</h3>
+        <div className="intrument-panel-col">
+          <div className="instrument-panel-row">
+            <InstrumentField
+              value={data.data.currentThrust}
+              tag="Power"
+              unit="%"
+              source="Azimuth"
+              hasSource={true}
+            ></InstrumentField>
+            <InstrumentField
+              value={data.data.currentAngle}
+              degree={true}
+              tag="Angle"
+              unit="°"
+              source="Azimuth"
+              hasSource={true}
+            ></InstrumentField>
+            <InstrumentField
+              value={simulatorData.data.rpm}
+              tag="RPM"
+              source="Simulator"
+              hasSource={true}
+            ></InstrumentField>
+            <InstrumentField
+              value={simulatorData.data.speed}
+              tag="Speed"
+              unit="kn"
+              source="Simulator"
+              hasSource={true}
+            />
+          </div>
+          <div className="instrument-panel-row">
+            <MemoizedAzimuthThruster
+              thrust={data.data.currentThrust}
+              angle={data.data.currentAngle}
+              setPoint={10}
+              touching={true}
+              atThrustSetpoint={false}
+              atAngleSetpoint={false}
+            />
+          </div>
+        </div>
+        <hr className="solid"></hr>
+        <h3>Navigation</h3>
+        <div className="instrument-panel-col">
+          <div className="instrument-panel-row">
+            <InstrumentField
+              setPoint={0}
+              hasSetPoint={true}
+              value={toHeading(simulatorData.data.heading)}
+              degree={true}
+              maxDigits={3}
+              fractionDigits={0}
+              tag="Heading"
+              unit="°"
+              source="Simulator"
+              hasSource={true}
+            ></InstrumentField>
+            <InstrumentField
+              setPoint={0}
+              hasSetPoint={true}
+              value={simulatorData.data.xPos}
+              degree={false}
+              maxDigits={3}
+              fractionDigits={1}
+              tag="X"
+              unit="m"
+              source="Simulator"
+              hasSource={true}
+            ></InstrumentField>
+            <InstrumentField
+              setPoint={0}
+              hasSetPoint={true}
+              value={simulatorData.data.yPos}
+              degree={false}
+              maxDigits={3}
+              fractionDigits={1}
+              tag="Y"
+              unit="m"
+              source="Simulator"
+              hasSource={true}
+            ></InstrumentField>
+          </div>
+          <div className="instrument-panel-row">
+            <MemoizedCompass
+              heading={simulatorData.data.heading}
+              courseOverGround={90}
+              headingAdvices={[]}
+            />
+          </div>
+        </div>
+        <hr className="solid"></hr>
+        {/* Others */}
+        <div className="info-card">
+          <h3>Eco Parameters</h3>
+          <div className="instrument-panel-row">
+            <InstrumentField
+              value={simulatorData.data.consumptionRate}
+              tag="Cons"
+              unit="kg/h"
+              source="Simulator"
+              hasSource={true}
+              maxDigits={4}
+              fractionDigits={1}
+            ></InstrumentField>
+            <InstrumentField
+              value={gramsToKiloGrams(simulatorData.data.consumedTotal)}
+              tag="ConsT"
+              unit="kg"
+              source="Simulator"
+              hasSource={true}
+              maxDigits={6}
+              fractionDigits={2}
+            ></InstrumentField>
+          </div>
+        </div>
+        <hr className="solid"></hr>
+
+        {/* Checkpoints */}
+        <div className="info-card">
+          <h3>Checkpoints</h3>
+          <p>Remaining: {simulatorData.data.checkpoints} out of 3</p>
+        </div>
+      </div>
     </div>
   )
 }
 
-// Memoized Components to prevent unnecessary re-renders
+// Memoized Component to prevent unnecessary re-renders
 const MemoizedAzimuthThruster = memo(AzimuthThruster)
-const MemoizedThruster = memo(Thruster)
-const MemoizedRudder = memo(Rudder)
+const MemoizedCompass = memo(Compass)
