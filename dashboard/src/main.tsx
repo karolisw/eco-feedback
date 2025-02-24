1f38bfcce3b869451cca7e3ce9c8f5d40081cea9
karoline <karoline.wahl@hotmail.no>
2025-02-24 13:02:11 +0100

    Context tells us whether the simulation is running or not.

--- dashboard/src/main.tsx
+++ dashboard/src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
 import './index.css'
 import App from './App'
+import { SimulationContextProvider } from './context/SimulationContextProvider'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <SimulationContextProvider>
+      <Router>
+        <App />
+      </Router>
+    </SimulationContextProvider>
   </StrictMode>
 )
