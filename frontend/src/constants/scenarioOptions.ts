f2bf79f62fba95bf4b4bc7272bd21b4499696db9
karoline <karoline.wahl@hotmail.no>
2025-03-27 14:49:57 +0100

    Four scenarios available for use in dropdown.

---
+++ frontend/src/constants/scenarioOptions.ts
@@ -0,0 +1,8 @@
+export type ScenarioKey = 'maintain-speed' | 'turn-around' | 'navigate-buoys' | 'depart-harbor'
+
+export const scenarioOptions: Record<ScenarioKey, string> = {
+    'maintain-speed': 'Maintain Speed',
+    'turn-around': 'Turn Around',
+    'navigate-buoys': 'Navigate Buoys',
+    'depart-harbor': 'Depart Harbor'
+  }
